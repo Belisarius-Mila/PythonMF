5da3482ca6fe0262a3b8ee93e50193287291cdd4
Mila <miloslav.falta2@icloud.com>
2026-05-09 09:16:16 +0200

    Refresh colors numbers owl audio

--- docs/colors-numbers/app.js
+++ docs/colors-numbers/app.js
@@ -135,7 +135,7 @@ const appState = {
   },
 };
 
-const owlAudio = new Audio("owl_080526.mp3?v=20260508a");
+const owlAudio = new Audio("owl_080526.mp3?v=20260509a");
 owlAudio.preload = "auto";
 
 function randomChoice(items) {
