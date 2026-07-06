e9f23f88de69ac95ca386272416be82de221ed67
Mila <miloslav.falta2@icloud.com>
2026-07-06 09:44:13 +0200

    Use unique July 6 owl audio URL

--- ColorsAndNumbers/web_colors_numbers/app.js
+++ ColorsAndNumbers/web_colors_numbers/app.js
@@ -135,7 +135,7 @@ const appState = {
   },
 };
 
-const owlAudio = new Audio("owl_060726.mp3?v=20260706a");
+const owlAudio = new Audio("owl_060726_katka.mp3?v=20260706b");
 owlAudio.preload = "auto";
 
 function randomChoice(items) {
