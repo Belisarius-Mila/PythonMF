f8d177b0be7fdcb07320410f1c3e6851fd704798
Mila <miloslav.falta2@icloud.com>
2026-07-24 06:27:57 +0200

    Update July 24 owl speech

--- ColorsAndNumbers/web_colors_numbers/app.js
+++ ColorsAndNumbers/web_colors_numbers/app.js
@@ -135,7 +135,7 @@ const appState = {
   },
 };
 
-const owlAudio = new Audio("owl_240726.mp3?v=20260724a");
+const owlAudio = new Audio("owl_240726.mp3?v=20260724b");
 owlAudio.preload = "auto";
 
 function randomChoice(items) {
