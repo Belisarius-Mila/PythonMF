64d1e79d8151933b364ac1994387409ea91bdd5e
Mila <miloslav.falta2@icloud.com>
2026-07-28 05:51:21 +0200

    Publish owl speech for 2026-07-28

--- docs/colors-numbers/app.js
+++ docs/colors-numbers/app.js
@@ -135,7 +135,7 @@ const appState = {
   },
 };
 
-const owlAudio = new Audio("owl_270726.mp3?v=20260727a");
+const owlAudio = new Audio("owl_280726.mp3?v=20260728a");
 owlAudio.preload = "auto";
 
 function randomChoice(items) {
